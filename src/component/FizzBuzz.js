92c866c9a3ccfe1f6f3990d65700c0b871c91205
CatAnderson <thecatanderson100@gmail.com>
2020-12-08 14:08:22 +0000

    got useEffect producing FizzBuzz

--- src/component/FizzBuzz.js
+++ src/component/FizzBuzz.js
@@ -1,8 +1,9 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const FizzBuzz = () => {
 
     const [number, setNumber] = useState(1);
+    const [result, setResult] = useState("");
     
     const addNumber = () => {
         setNumber(number + 1);
@@ -16,10 +17,23 @@ const FizzBuzz = () => {
         }
     };
 
+    useEffect(() =>{
+        if (number % 5 == 0 && number % 3 == 0){
+            setResult("FizzBuzz")
+        } else if (number % 5 == 0){
+            setResult("Buzz")
+            } else if(number % 3 == 0){
+                setResult("Fizz")
+            } else {
+                setResult(number)
+            }
+        }, [number])
+
     return(
         <>
             <h1>FizzBuzz</h1>
             <h2>{ number }</h2>
+            <h3>{result}</h3>
             <button onClick={() => addNumber()}>+1</button>
             <button onClick={() => minusNumber()}>-1</button>
         </>
