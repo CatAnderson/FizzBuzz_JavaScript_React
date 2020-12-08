c74e49174ec71ce4a51050625fff84642855fcff
CatAnderson <thecatanderson100@gmail.com>
2020-12-08 13:47:34 +0000

    added state and function to do onClick

--- src/component/FizzBuzz.js
+++ src/component/FizzBuzz.js
@@ -1,10 +1,23 @@
+import { useState } from "react";
+
 const FizzBuzz = () => {
+
+    const [number, setNumber] = useState(1);
+    
+    const addNumber = () => {
+        setNumber(number + 1);
+    };
+
+    const minusNumber = () => {
+        setNumber(number - 1);
+    };
+
     return(
         <>
-        <h1>FizzBuzz</h1>
-        <h2>Number</h2>
-        <button>+1</button>
-        <button>-1</button>
+            <h1>FizzBuzz</h1>
+            <h2>{ number }</h2>
+            <button>+1</button>
+            <button>-1</button>
         </>
     )
 };
