2b7c3411217f292fe7a7a7b082f76f24f33d3504
CatAnderson <thecatanderson100@gmail.com>
2020-12-08 13:40:54 +0000

    basics done, can see FizzBuzz component in browser

---
+++ src/component/FizzBuzz.js
@@ -0,0 +1,12 @@
+const FizzBuzz = () => {
+    return(
+        <>
+        <h1>FizzBuzz</h1>
+        <h2>Number</h2>
+        <button>+1</button>
+        <button>-1</button>
+        </>
+    )
+};
+
+export default FizzBuzz;
